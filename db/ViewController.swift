a9e1aeef9ca0fa781a64fcc91138801b64cbc030
deni zakya <denizakya@gmail.com>
2023-09-08 22:24:52 +0200

    Initial Commit

---
+++ db/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  db
+//
+//  Created by deni zakya on 08/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
